76b8818109eafe747e9dad63b060ae6fb7af02c9
Rachid <rachid.aifar24@gmail.com>
2021-10-10 23:38:58 +0200

    feat:registration form backend has been created

--- Website/server.php
+++ Website/server.php
@@ -13,3 +13,42 @@ $errors = array();
 
 // connect to db
 $db = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+// if the user clicks on the register button
+if(isset($_POST['register'])){
+    // get the user input
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $email = mysqli_real_escape_string($db, $_POST['email']);
+    $password = mysqli_real_escape_string($db, $_POST['password']);
+    $confirm_password = mysqli_real_escape_string($db, $_POST['confirm_password']);
+
+    // check whether the passwords match of not
+    if($password != $confirm_password){
+       array_push($errors, "password must match !");
+      
+    }
+
+
+    //check if both of the username and password are unique or not
+    $query = "SELECT * FROM user WHERE username = '$username' OR email = '$email' LIMIT 1";
+    $result = mysqli_query($db, $query); // mysqli_query method to execute the query 
+    $user = mysqli_fetch_assoc($result); // get an assocciative array of the query
+
+    if($user) 
+    {
+        if($user['username'] === $username){
+            array_push($errors, "username already exist !");
+        }
+        if($user['email'] === $email){
+            array_push($errors, "email already exist !");
+        }
+    }
+    //if there are no errors we insert the user to db
+    if(count($errors) == 0) {
+        // $encrypted_passsword = md5($password);
+        $insert = "INSERT INTO user (username, email, password) VALUES ('$username', '$email', '$passsword')";
+        mysqli_query($db, $insert);
+        $_SESSION["username"] = $username;
+        $_SESSION['success'] = 'success';
+        header("Location: Registration.php");
+    }
+}
