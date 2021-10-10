3d6fbec24297bb363fdb0a5a074025fd597529a2
Rachid <rachid.aifar24@gmail.com>
2021-10-10 23:41:11 +0200

    faet: errors php file has been created

---
+++ Website/errors.php
@@ -0,0 +1,7 @@
+<?php if(count($errors) > 0): ?>
+    <div class = "errors">
+        <?php foreach($errors as $errors) :?>
+        <p><?php echo $errors?></p>
+        <?php endforeach ?>
+    </div>
+<?php endif ?>
