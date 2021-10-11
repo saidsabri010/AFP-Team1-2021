2ab44ec72435e80f2690c7935caba63ead689eac
dany <danirazzouqa@gmail.com>
2021-10-12 01:29:56 +0200

    Service : CERTIFIED TEACHERS / Front End

--- Website/service.php
+++ Website/service.php
@@ -19,9 +19,11 @@
 
         <div class="content1">
         <h3 class ="t1">SPECIAL EDUCATION</h3>
-        <p class="p1"> Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+        <p class="p1"> Special education serves children with emotional, behavioral, or cognitive impairments or with intellectual, hearing, vision, speech, or learning disabilities; gifted children with advanced academic abilities; and children with orthopedic or neurological impairments..</p>
         <h3 class ="t1">ONLINE EDUCATION </h3>
-        <p class="p1"> Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+        <p class="p1"> With online education, students can turn anywhere with Internet access and electricity into a classroom. It can include audio, video, text, animations, virtual training environments and live chats with professors. It's a rich learning environment with much more ﬂexibility than a traditional classroom.</p>
+        <h3 class ="t1">CERTIFIED TEACHERS</h3>
+        <p class="p1"> eacher certification ensures that an individual is competent in both the subject matter and the general practice of teaching a certain audience. That is why it is crucial for students to enroll in an accredited degree program.</p>
         
       </div>
      
