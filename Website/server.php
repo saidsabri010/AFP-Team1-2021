845a476f0b84f95fec3191af261ea6c9528ae7a4
dany <danirazzouqa@gmail.com>
2021-10-12 00:00:57 +0200

    Feat : Login Backend

--- Website/server.php
+++ Website/server.php
@@ -45,10 +45,30 @@ if(isset($_POST['register'])){
     //if there are no errors we insert the user to db
     if(count($errors) == 0) {
         // $encrypted_passsword = md5($password);
-        $insert = "INSERT INTO user (username, email, password) VALUES ('$username', '$email', '$passsword')";
+        $insert = "INSERT INTO user (username, email, password) VALUES ('$username', '$email', '$password')";
         mysqli_query($db, $insert);
         $_SESSION["username"] = $username;
         $_SESSION['success'] = 'success';
         header("Location: Registration.php");
     }
 }
+if(isset($_POST['login'])){
+
+    $email=mysqli_real_escape_string($db, $_POST['email']);
+    $password=mysqli_real_escape_string($db,$_POST['password']);
+
+    $query="SELECT * FROM user WHERE email='$email' AND password='$password'";
+    $result=mysqli_query($db,$query);
+    if(count($errors)==0)
+     {
+        if(mysqli_num_rows($result)==1){
+            $_SESSION["username"] = $username;
+            $_SESSION['success'] = 'success';
+            header("Location: profile.php");
+        }else{
+                array_push($errors, "login failed. wrong password or username");
+            }
+          
+    }
+}
+?>
